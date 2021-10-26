d885cd31852d2d50c71135993e2cd4c5f96fcba2
Wanda <cwand07@gmail.com>
2021-10-26 19:11:40 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+// import imagen from './assets/favicon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hola Coders!</Text>
+      <Image
+        source={{ uri: 'https://picsum.photos/200/200' }}
+        style={styles.image}
+      />
+      {/* <Image source={imagen} /> */}
+      <Button title='Click me' color='#000' />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,8 +23,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a8dadc',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  title: { fontSize: 35, fontWeight: 'bold' },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    marginTop: 20,
+    marginBottom: 15,
   },
 });
