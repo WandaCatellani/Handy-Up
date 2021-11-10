896d76b2171a54413d5fd58d2933c104117e878b
Wanda <cwand07@gmail.com>
2021-11-09 23:43:17 -0300

    Add core components & styles

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+import Home from './src/screens/Home/Home';
+// import Colors from './src/constants/colors';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-
-// import imagen from './assets/favicon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hola Coders!</Text>
-      <Image
-        source={{ uri: 'https://picsum.photos/200/200' }}
-        style={styles.image}
-      />
-      {/* <Image source={imagen} /> */}
-      <Button title='Click me' color='#000' />
-      <StatusBar style='auto' />
+      <Home />
     </View>
   );
 }
@@ -23,17 +16,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#a8dadc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: { fontSize: 35, fontWeight: 'bold' },
-  image: {
-    height: 200,
-    width: 200,
-    borderRadius: 100,
-    marginTop: 20,
-    marginBottom: 15,
   },
 });
