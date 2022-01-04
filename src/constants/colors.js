964326f98538c3cea44c6238d19326e093fe6099
Wanda <cwand07@gmail.com>
2022-01-04 02:45:20 -0300

    Add images

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -5,4 +5,9 @@ export default {
   backgroundPrimary: '#3473a7',
   white: '#fcfcfc',
   black: '#111111',
+  orange: '#f27524',
+  gray: '#828282',
+  grayMedium: '#9c9c9c',
+  grayLight: '#efeff4',
+  blue: '#284285',
 };
