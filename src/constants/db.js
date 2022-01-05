14f6805ce1d090ff7757a4107c326ce676fdb615
Wanda <cwand07@gmail.com>
2022-01-05 18:52:45 -0300

    Publish & Build

--- src/constants/db.js
+++ src/constants/db.js
@@ -1,10 +1,7 @@
 export const API_URL = 'https://serviceapp-6c4d8-default-rtdb.firebaseio.com';
+
 export const SIGN_UP_URL =
-  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjXy_zdm8ajZzzRuySR4g9c_UGXcRFWXQ';
-export const URL_AUTH_API =
   'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjXy_zdm8ajZzzRuySR4g9c_UGXcRFWXQ';
 
 export const SIGN_IN_URL =
-  'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBjXy_zdm8ajZzzRuySR4g9c_UGXcRFWXQ';
-export const URL_LOGIN_API =
   'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBjXy_zdm8ajZzzRuySR4g9c_UGXcRFWXQ';
